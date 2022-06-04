6afe261757c70c29eac254dca20ef6b520a2af76
harry19851201 <76029086+harry19851201@users.noreply.github.com>
2022-06-05 01:09:44 +0800

    Update signin.py

--- signin.py
+++ signin.py
@@ -36,8 +36,8 @@ def run(form_data):
             login_url = login_url.replace("amp;", "")
             print(login_url)
     form_text = re.search('formhash=(.*?)\'', user_resp.text)
-    print(form_text.group(1))
-    form_data['formhash'] = form_text.group(1)
+    print(form_text.group)
+    form_data['formhash'] = form_text.group
     print(form_data)
 
     login_resp = s.post(login_url, data=form_data)
@@ -47,7 +47,7 @@ def run(form_data):
     else:
       return 'login failed!'
     signin_text = re.search('formhash=(.*?)"', test_resp.text)
-    signin_resp = s.get(signin_url.format(formhash=signin_text.group(1)))
+    signin_resp = s.get(signin_url.format(formhash=signin_text.group))
     test_resp = s.get('https://www.4ksj.com/k_misign-sign.html',headers=headers)
     if '您的签到排名' in test_resp.text:
       print('signin!')
